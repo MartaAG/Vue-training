7aff505aa51c90a347dc3f25005d056e819e8ee0
Marta G <marta.guszek@gmail.com>
2020-07-14 14:15:15 +0200

    add go-back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="goBack">go back</button>
+</template>
+
+<script>
+  export default {
+    name: 'BackButton',
+    methods: {
+      goBack() {
+        return this.$router.go(-1);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  button {
+    cursor: pointer;
+  }
+</style>
--- src/views/Paragraph.vue
+++ src/views/Paragraph.vue
@@ -2,15 +2,19 @@
   <div>
     <h2>{{this.$route.params.id}}</h2>
     <lorem add="1p"></lorem>
+    <BackButton />
   </div>
+
 </template>
 
 <script>
   import LoremIpsum from 'vue-lorem-ipsum';
+  import BackButton from "@/components/BackButton"
   export default {
     name: 'Paragraph',
     components: {
-      'lorem': LoremIpsum
+      'lorem': LoremIpsum,
+      BackButton
     }
   }
 
